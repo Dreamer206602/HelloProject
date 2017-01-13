3b51c844bea85849914ebcea407b23a86c320a43
Dreamer206602 <boobooMX@163.com>
2017-01-13 17:13:03 +0800

    高度 宽度 文本输入  ScrollView的学习

--- README.md
+++ README.md
@@ -1,60 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Image
-} from 'react-native';
-
-export default class HelloProject extends Component {
-  render() {
-    let pic={
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-    return (
-//      <Image source={pic} style={{width:193,height:110}} />
-       <View style={styles.container}>
-         <Text style={styles.welcome}>
-         HelloProject    
-         </Text>
-         <Text style={styles.instructions}>
-           To get started, edit index.android.js
-         </Text>
-         <Text style={styles.instructions}>
-           Double tap R on your keyboard to reload,{'\n'}
-           Shake or press menu button for dev menu
-         </Text>
-       </View>
-    );
-  }
-}
+## Flex Direction
+在组件的style中指定flexDirection可以决定布局的主轴。子元素是应该沿着水平轴(row)方向排列，还是沿着竖直轴(column)方向排列呢？默认值是竖直轴(column)方向。
 
 
+## Justify Content
+在组件的style中指定justifyContent可以决定其子元素沿着主轴的排列方式。子元素是应该靠近主轴的起始端还是末尾段分布呢？亦或应该均匀分布？对应的这些可选项有：flex-start、center、flex-end、space-around以及space-between。
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+## Align Items
+在组件的style中指定alignItems可以决定其子元素沿着次轴（与主轴垂直的轴，比如若主轴方向为row，则次轴方向为column）的排列方式。子元素是应该靠近次轴的起始端还是末尾段分布呢？亦或应该均匀分布？对应的这些可选项有：flex-start、center、flex-end以及stretch。
+ 注意：要使stretch选项生效的话，子元素在次轴方向上不能有固定的尺寸。以下面的代码为例：只有将子元素样式中的width: 50去掉之后，alignItems: 'stretch'才能生效.
 
-AppRegistry.registerComponent('HelloProject', () => HelloProject);
+## 处理文本的输入
+TextInput是一个允许用户输入文本的基础组件。它有一个名为onChangeText的属性，此属性接受一个函数，而此函数会在文本变化时被调用。另外还有一个名为onSubmitEditing的属性，会在文本被提交后（用户按下软键盘上的提交键）调用。
+
+
+## 如何使用ScrollView
+ScrollView是一个通用的可滚动的容器，你可以在其中放入多个组件和视图，而且这些组件并不需要是同类型的。ScrollView不仅可以垂直滚动，还能水平滚动（通过horizontal属性来设置）。
--- index.android.js
+++ index.android.js
@@ -10,7 +10,9 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+    TextInput,
+    ScrollView
 } from 'react-native';
 
 
@@ -19,10 +21,13 @@ render(){
 
 return (
 
-    <View>
+    // 试试去掉父View中的`flex: 1`。
+    // 则父View不再具有尺寸，因此子组件也无法再撑开。
+    // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+    <View style={{height:300}}>
    <View style={{width:50,height:50,backgroundColor:'powderblue'}}/>
       <View style={{width:100,height:100,backgroundColor:'skyblue'}}/>
-//   <View style={{width:150,height:150,backgroundColor:'steeblue'}} />
+        <View style={{width:150,height:150,backgroundColor:'red'}} />
 
 
 
@@ -33,6 +38,122 @@ return (
 }
 
 };
+
+class  FlexDirectionBasics extends Component{
+
+    render(){
+
+        return(
+
+            // 尝试把`flexDirection`改为`column`看看
+            <View style={{flex:1,flexDirection:'column'}}>
+                <View style={{width:50,height:50,backgroundColor:'powderblue'}}/>
+                    <View style={{width:50,height:50,backgroundColor:'red'}}/>
+                        <View style={{width:50,height:50,backgroundColor:'black'}}/>
+
+                    </View>
+        );
+
+    }
+};
+
+
+class JustifyContentBasics extends Component{
+
+    render(){
+
+        // 尝试把`justifyContent`改为`center`看看
+        // 尝试把`flexDirection`改为`row`看看 column（默认）
+        return(
+            <View style={{flex:1,flexDirection:'row',justifyContent:'center'}}>
+                <View style={{width:50,height:50,backgroundColor:'powderblue'}}/>
+                <View style={{width:50,height:50,backgroundColor:'red'}}/>
+                <View style={{width:50,height:50,backgroundColor:'black'}}/>
+
+            </View>
+
+        );
+
+    }
+
+};
+
+
+class  AlignItemsBasics extends Component{
+    render(){
+
+        return(
+            // 尝试把`alignItems`改为`flex-start` center看看
+            // 尝试把`justifyContent`改为`flex-end` center flex-start, 看看
+            // 尝试把`flexDirection`改为`row`看看
+            // 注意：要使stretch选项生效的话，子元素在次轴方向上不能有固定的尺寸。以下面的代码为例：只有将子元素样式中的width: 50去掉之后，alignItems: 'stretch'才能生效。
+            <View style={{flex:1,flexDirection:'row',justifyContent:'flex-start',alignItems:'flex-start'}}>
+                <View style={{width:50,height:50,backgroundColor:'powderblue'}}/>
+                <View style={{width:50,height:50,backgroundColor:'red'}}/>
+                <View style={{width:50,height:50,backgroundColor:'black'}}/>
+            </View>
+
+        );
+
+    }
+};
+
+class  PizzaTranslator extends Component{
+    constructor(props){
+        super(props);
+        this.state={text:''};
+    }
+    render(){
+        return(
+            <View style={{padding:10}}>
+                <TextInput
+                    style={{height:40}}
+                    placeholder="Type here to translate"
+                    onChangeText={(text)=>this.setState({text})}
+                />
+                <Text style={{padding:10,fontSize:42}}>{this.state.text.split(' ').map((word) => word && '🍕').join(' ')}</Text>
+
+            </View>
+
+        );
+}
+};
+
+
+class  IScrolledDownAndWhatHappenedNextShockedMe extends Component{
+    render(){
+        return(
+            <ScrollView>
+                <Text style={{fontSize:36}}>Scroll me plz</Text>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Text style={{fontSize:46}}>Scrolling down</Text>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Text style={{fontSize:56}}>FrameWork around?</Text>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Image source={require('./image/hot.png')}/>
+                <Text style={{fontSize:20}}>React Native</Text>
+            </ScrollView>
+
+        );
+    }
+
+
+};
+
 
 
 
@@ -68,14 +189,9 @@ export default class HelloProject extends Component {
   }
 }
 
-
+AppRegistry.registerComponent('HelloProject', () => IScrolledDownAndWhatHappenedNextShockedMe);
 
 const styles = StyleSheet.create({
-
-
-
-
-
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -111,9 +227,3 @@ const styles = StyleSheet.create({
 
 });
 
-
-
-
-
-
-AppRegistry.registerComponent('HelloProject', () => FixedDimensionsBasics);
